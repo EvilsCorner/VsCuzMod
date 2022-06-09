436ba9560b6688ea6aeb5f0d8cb72b55ea9cf6af
Nichole Smith <smithnichole2016@gmail.com>
2022-06-08 21:59:32 -0600

    added gallery

--- export/release/windows/haxe/ApplicationMain.hx
+++ export/release/windows/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "36";
+		app.meta["build"] = "37";
 		app.meta["company"] = "ShadowMario";
 		app.meta["file"] = "PsychEngine";
 		app.meta["name"] = "Friday Night Funkin': Psych Engine";
